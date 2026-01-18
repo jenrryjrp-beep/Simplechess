3e4a0d1057cb456c046cf627f551ea180985130d
jenrryjrp-beep <jenrryjrp@gmail.com>
2026-01-18 19:37:10 -0400

    Create index.ts

---
+++ server/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const server = createServer(app);
+
+app.use(express.static(join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '../dist/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
