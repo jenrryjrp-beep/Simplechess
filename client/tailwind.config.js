ca45eadf18aa5f94e82a5a33580c214eb15b080b
jenrryjrp-beep <jenrryjrp@gmail.com>
2026-01-18 19:32:30 -0400

    Create tailwind.config.js

---
+++ client/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./client/src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'simple-black': '#1a1a1a',
+        'simple-pink': '#ffb3ba',
+        'simple-pink-hover': '#ff9aa2',
+      },
+    },
+  },
+  plugins: [],
+};
